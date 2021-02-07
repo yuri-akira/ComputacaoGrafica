c556e0eb87461423f1d4caaf2a7235132508c68f
Yuri Akira Shiba <61916114+yuri-akira@users.noreply.github.com>
2021-02-07 17:05:54 -0300

    Update spheres.cpp

--- iluminacao/spheres.cpp
+++ iluminacao/spheres.cpp
@@ -4,22 +4,21 @@
 #include <GL/glut.h>
 #endif
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-
-/* GLUT callback Handlers */
-GLfloat light0_pos[] = {2.0f,2.0f,2.0f,1.0f};
+GLfloat light0_pos[] = {2.0f, 2.0f, 2.0f, 1.0f};
 GLfloat white[] = {1.0f, 1.0f, 1.0f, 1.0f};
 GLfloat black[] = {0.0f, 0.0f, 0.0f, 1.0f};
 
-GLfloat angle = 0.0f;
-GLfloat step = 3.0f;
+GLfloat angleX = 0.0f;
+GLfloat angleY = 0.0f;
+GLfloat angleZ = 0.0f;
 GLfloat mouseX = 0.0;
 GLfloat mouseY = 0.0;
 
-void lanterna()
-{
+void lightning(){
     glLightfv(GL_LIGHT0, GL_POSITION,light0_pos);
     glLightfv(GL_LIGHT0, GL_AMBIENT, black);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, black);
@@ -43,20 +42,6 @@ void lanterna()
     glEnable(GL_LIGHT1);
 }
 
-void mouseFunc(int botao, int estado, int x, int y){
-    switch(botao){
-    case GLUT_LEFT_BUTTON:
-        if(estado== GLUT_DOWN){
-            mouseX=(((float)x/((float)500/2))-1)*2;
-            mouseY=-(((float)y/((float)500/2))-1)*2;
-        }
-                break;
-    default:
-        break;
-    }
-    glutPostRedisplay();
-}
-
 
 void init(){
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -72,12 +57,37 @@ void init(){
     glLoadIdentity();
     glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
     glViewport(0, 0, 500, 500);
-    lanterna();
+    lightning();
     glPushMatrix();
 }
 
-void displayFunc(){
-GLfloat diffuse[4];
+void cube(){
+    GLfloat shininess;   //Coeficiente de reflexão da luz ambiente k_a
+    GLfloat diffuse[4];  //Coeficiente de reflexão difusa k_d
+    GLfloat specular[4]; //Coeficiente de reflexão especular k_s
+
+    shininess = 65.0;
+    diffuse[0] = 0.65;
+    diffuse[1] = 0.0;
+    diffuse[2] = 0.0;
+    diffuse[3] = 1.0;
+    specular[0] = 1.0;
+    specular[1] = 1.0;
+    specular[2] = 1.0;
+    specular[3] = 1.0;
+    glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,diffuse);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,specular);
+    glMaterialf(GL_FRONT,GL_SHININESS,shininess);
+
+    glutSolidSphere(0.25,40,40);
+
+    glEnd();
+}
+
+void displayFunc() {
+        GLfloat diffuse[4];
+        GLfloat specular[4];
+        GLfloat ns;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa o buffer
 
         glMatrixMode(GL_MODELVIEW); //define que a matrix È a model view
@@ -87,54 +97,71 @@ GLfloat diffuse[4];
                   0.0, 1.0, 0.0);  //vetor view-up
 
         glPushMatrix();
+        glRotatef(0.0,1.0,0.0,0.0);
+        glRotatef(0.0,0.0,1.0,0.0);
+        glRotatef(angleZ,0.0,0.0,1.0);
 
-        glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,diffuse);
+        glPushMatrix();
+        glTranslatef(-1.5,0.0,0.0);
+        cube();
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef (1.5,0.0,0.0);
+        cube();
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(0.0,-1.5,0.0);
+        cube();
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(0.0,1.5,0.0);
+        cube();
+        glPopMatrix();
+
+        glPopMatrix();
+
+        lightning();
+
+        glFlush();
 }
 
-void drawCube(){
-     glColor3f(1.0,0.0,0.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(-0.5,-0.5,0.5);
-        glVertex3f(0.5,-0.5,0.5);
-        glVertex3f(0.5,0.5,0.5);
-        glVertex3f(-0.5,0.5,0.5);
-     glEnd();
-     glColor3f(0.0,1.0,0.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(0.5,0.5,0.5);
-        glVertex3f(0.5,-0.5,0.5);
-        glVertex3f(0.5,-0.5,-0.5);
-        glVertex3f(0.5,0.5,-0.5);
-     glEnd();
-     glColor3f(0.0,0.0,1.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(0.5,-0.5,0.5);
-        glVertex3f(-0.5,-0.5,0.5);
-        glVertex3f(-0.5,-0.5,-0.5);
-        glVertex3f(0.5,-0.5,-0.5);
-     glEnd();
-     glColor3f(1.0,1.0,0.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(-0.5,0.5,0.5);
-        glVertex3f(0.5,0.5,0.5);
-        glVertex3f(0.5,0.5,-0.5);
-        glVertex3f(-0.5,0.5,-0.5);
-     glEnd();
-     glColor3f(0.0,1.0,1.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(-0.5,-0.5,-0.5);
-        glVertex3f(-0.5,0.5,-0.5);
-        glVertex3f(0.5,0.5,-0.5);
-        glVertex3f(0.5,-0.5,-0.5);
-     glEnd();
-     glColor3f(1.0,1.0,1.0);
-     glBegin(GL_POLYGON);
-        glVertex3f(-0.5,0.5,-0.5);
-        glVertex3f(-0.5,-0.5,-0.5);
-        glVertex3f(-0.5,-0.5,0.5);
-        glVertex3f(-0.5,0.5,0.5);
-     glEnd();
+void keyboard (unsigned char key, int x, int y){
+    switch(key){
+       case 'x':
+       case 'X':
+            angleX+=5.0f;
+          break;
+       case 'y':
+       case 'Y':
+            angleY+=5.0f;
+          break;
+      case 'z':
+      case 'Z':
+            angleZ+=5.0f;
+          break;
+      default:
+          break;
+    }
+    glutPostRedisplay();
 }
+
+void mouseFunc(int botao, int estado, int x, int y){
+    switch(botao){
+    case GLUT_LEFT_BUTTON:
+        if(estado== GLUT_DOWN){
+            mouseX=(((float)x/((float)500/2))-1)*2;
+            mouseY=-(((float)y/((float)500/2))-1)*2;
+        }
+                break;
+    default:
+        break;
+    }
+    glutPostRedisplay();
+}
+
 
 int main(int argc, char *argv[]){
     glutInit(&argc,argv);
@@ -143,6 +170,8 @@ int main(int argc, char *argv[]){
     glutInitWindowSize(500,500);
     glutCreateWindow("Iluminacao");
     glutDisplayFunc(displayFunc);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouseFunc);
     init();
     glutMainLoop();
     return 0;
